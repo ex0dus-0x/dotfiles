4009e96b55ea04dcff38e3a21f4596ad82e9e8f9
ex0dus-0x <ex0dus@codemuch.tech>
2021-01-02 19:07:04 -0500

    Refactor polybar and rofi

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,12 +1,14 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/polybar"
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar top &
-
-echo "Bars launched..."
+# Launch the bar
+polybar -q main -c "$DIR"/config.ini &
